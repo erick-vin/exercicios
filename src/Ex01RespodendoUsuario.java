b16bab056b9e786776cd3ceed621f62f90ac9ff0
Erick Vinicius <ericktjdev@gmail.com>
2021-06-12 00:17:51 -0300

    Projeto criado

---
+++ src/Ex01RespodendoUsuario.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Ex01RespodendoUsuario {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		String nome;
+		
+		System.out.print("Digite o seu nome: ");
+		nome = sc.next();
+		
+		System.out.println("Muito Prazer em te conhecer " + nome);
+		
+		sc.close();
+	}
+
+}
