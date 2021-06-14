34f537d235f1a1b994caec7ee697342c5a1598da
Erick Vinicius <ericktjdev@gmail.com>
2021-06-14 11:36:26 -0300

    Criado Exercicio Ex004

---
+++ src/Ex004MediaAlunos.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Ex004MediaAlunos {
+
+	public static void main(String[] args) {
+		/*O Programa lê duas notas de um aluno
+		 * e calcula sua média e diz se o aluno 
+		 * foi aprovado ou reprovado,
+		 * media de aprovação maior ou igual a 7*/
+		
+		Scanner sc = new Scanner(System.in);
+		
+		double nota1, nota2, media;
+		
+		System.out.println("================== \n"
+			           	 + " MEDIA DOS ALUNOS \n"
+			           	 + "================== \n");
+		
+		System.out.print("Digite a Primeira nota: ");
+		nota1 = sc.nextDouble();
+		
+		System.out.print("Digite a Segunda nota: ");
+		nota2 = sc.nextDouble();
+		
+		media = (nota1 + nota2) / 2;
+		
+		System.out.printf("O aluno deve media %.2f \n", media);
+		
+		if (media >= 7) {
+			System.out.println("Ele foi APROVADO!");
+		}
+		else {
+			System.out.println("Ele foi REPROVADO!");
+		}
+		
+		
+		sc.close();
+
+	}
+
+}
